c95a2a710c955648d1e8d886aecaff8fd59039be
Ifeoluwa Olawoye <dev@hostvillenigeria.org>
2020-03-21 12:27:01 +0100

    differentiated queues redis

--- config/queue.php
+++ config/queue.php
@@ -60,7 +60,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'default',
+            'queue' => env('REDIS_QUEUE'),
             'retry_after' => 90,
         ],
 
