5a81c303fe297ce3ef7d2fd8e78fa7c282a3ab83
tomideo1 <ayotomideaina@gmail.com>
2021-01-04 15:25:53 +0100

    analytics merge

---
+++ database/migrations/2020_12_20_1608476262_create_tax_authorities_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxAuthoritiesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tax_authorities', function (Blueprint $table) {
+
+		$table->increments(id)->unsigned();
+		$table->string('authority_name');
+		$table->enum('payment_mode',['Paystack','FlutterWave']);
+		;
+		$table->timestamp('created_at')->nullable()->default('NULL');
+		$table->timestamp('updated_at')->nullable()->default('NULL');
+		$table->primary('id');
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tax_authorities');
+    }
+}
