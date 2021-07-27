c5181442dc6d361c4cdd46dcee31a4ba5d61023f
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-07-27 13:13:00 +0100

    initialized Dorcas Setup handle

--- app/Console/Commands/DorcasSetup.php
+++ app/Console/Commands/DorcasSetup.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use Illuminate\Support\Facades\DB;
+
 class DorcasSetup extends Command
 {
     /**
@@ -35,8 +37,33 @@ class DorcasSetup extends Command
      *
      * @return mixed
      */
-    public function handle()
+    public function handle($options = "")
     {
-        //
+        if (empty($options)) {
+            // default setup
+            $database = env('DB_DATABASE', false);
+
+            if (!$database) {
+                $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
+                return;
+            }
+    
+            try {
+                //putenv ("CUSTOM_VARIABLE=hero");
+
+                //Connecting to MySQL
+                $pdo = DB::connection()->getPdo(env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'));
+    
+                //Creating the Database
+                $pdo->exec(sprintf(
+                    'CREATE DATABASE IF NOT EXISTS %s ;', $database
+                ));
+    
+                $this->info(sprintf('Successfully created %s database', $database));
+    
+            } catch (PDOException $exception) {
+                $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
+            }
+        }
     }
 }
