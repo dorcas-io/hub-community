1deaaf6385ae86b38fd5f75b3397372b9f4b0d30
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-07-27 15:10:55 +0100

    setup dbname fix

--- app/Console/Commands/DorcasSetup.php
+++ app/Console/Commands/DorcasSetup.php
@@ -41,7 +41,7 @@ class DorcasSetup extends Command
     {
         if (empty($options)) {
             // default setup
-            $database = env('DB_DATABASE', false);
+            $database = env('DB_DATABASE', "business-hub");
 
             if (!$database) {
                 $this->info('Skipping creation of database as env(DB_DATABASE) is empty');
