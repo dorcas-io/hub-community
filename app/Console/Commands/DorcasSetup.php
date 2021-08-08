1a56433b329ef5866ad43c4c71967e4549b3744f
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-08-08 09:59:34 +0100

    added core db donnection; App Key and OAuth Client writing to env

--- app/Console/Commands/DorcasSetup.php
+++ app/Console/Commands/DorcasSetup.php
@@ -52,7 +52,45 @@ class DorcasSetup extends Command
 
         // DATABASE CREATION AND IMPORT MOVED TO CORE DORCAS SETUP COMMAND
 
+        $this->info('Writing OAuth Client Details to .env');
+        try {
+            $client = DB::table("oauth_clients")->where('password_client', 1)->first();
+            $client_id = $client->id;
+            $client_secret = $client->secret;
 
+            $this->info(' ID: ' . $client_id . " / Secret: " . $client_secret);
+
+            $path = base_path('.env');
+            if (file_exists($path)) {
+                file_put_contents($path, str_replace(
+                    'DORCAS_CLIENT_ID=', 'DORCAS_CLIENT_ID='.$client_id, file_get_contents($path)
+                ));
+                file_put_contents($path, str_replace(
+                    'DORCAS_CLIENT_SECRET=', 'DORCAS_CLIENT_SECRET='.$client_secret, file_get_contents($path)
+                ));
+                file_put_contents($path, str_replace(
+                    'DORCAS_PERSONAL_CLIENT_ID=', 'DORCAS_PERSONAL_CLIENT_ID='.$client_id, file_get_contents($path)
+                ));
+                file_put_contents($path, str_replace(
+                    'DORCAS_PERSONAL_CLIENT_SECRET=', 'DORCAS_PERSONAL_CLIENT_SECRET='.$client_secret, file_get_contents($path)
+                ));
+                $this->info('Successfully written Client ID & Secret to .env');
+            }
+
+
+        } catch (Exception $exception) {
+            $this->error(sprintf('Failed setting up OAuth: %s', $exception->getMessage()));
+        }
+
+        $this->info('Creating Laravel App Key...');
+        $key = \Illuminate\Support\Str::random(32);
+        $path = base_path('.env');
+        if (file_exists($path)) {
+            file_put_contents($path, str_replace(
+                'APP_KEY=', 'APP_KEY='.$key, file_get_contents($path)
+            ));
+            $this->info('Successfully written App Key to .env');
+        }
 
 
     }
--- config/database.php
+++ config/database.php
@@ -58,6 +58,24 @@ return [
             ) : [],  
 
         ],
+        'core_mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_CORE_HOST', '127.0.0.1'),
+            'port' => env('DB_CORE_PORT', '3306'),
+            'database' => env('DB_CORE_DATABASE', 'forge'),
+            'username' => env('DB_CORE_USERNAME', 'forge'),
+            'password' => env('DB_CORE_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => env('DB_CORE_PREFIX', ''),
+            'strict' => true,
+            'engine' => null,
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => env('DB_SSLMODE') == "require" && extension_loaded('pdo_mysql') && !empty(env('MYSQL_ATTR_SSL_CA')) ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
         'dorcas-api' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
